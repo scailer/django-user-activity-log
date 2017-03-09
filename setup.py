24f655e04de9e414b7afaf92241007a695ee5d33
sebatyler <sebatyler@gmail.com>
2017-03-09 18:27:07 +0900

    version up

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ More: https://github.com/sebatyler/django-user-activity-log
 
 setup(
     name='django-user-activity-log2',
-    version='0.0.16',
+    version='0.0.17',
     author='sebatyler',
     author_email='sebatyler@gmail.com',
 
