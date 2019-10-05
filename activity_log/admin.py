b3aac26ad054cadc02199d4a84808760d5063304
sebatyler <sebatyler@gmail.com>
2019-10-05 19:13:13 +0200

    Improve admin

--- activity_log/admin.py
+++ activity_log/admin.py
@@ -9,9 +9,9 @@ from . import models
 class LogAdmin(admin.ModelAdmin):
     list_display = ('user', 'user_id', 'request_method', 'request_url',
                     'response_code', 'datetime', 'ip_address')
-    date_hierarchy = 'datetime'
     list_filter = ('request_method', 'response_code')
     search_fields = ('user', 'request_url', '^ip_address')
+    show_full_result_count = False
 
 
 admin.site.register(models.ActivityLog, LogAdmin)
